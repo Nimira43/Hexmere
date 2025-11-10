5f0a65e424cbbace83825296dfe2376a12b1e83e
Nimira43 <n.rabone72@aol.com>
2025-11-10 19:43:40 +0000

    Updated declarations.d.ts

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,2 +1,10 @@
 // declarations.d.ts
-declare module 'three/addons/misc/Timer.js';
+declare module 'three/addons/misc/Timer.js' {
+  export default class Timer {
+    start(): void;
+    stop(): void;
+    update(): void;
+    getElapsed(): number;
+    getDelta(): number;
+  }
+}
