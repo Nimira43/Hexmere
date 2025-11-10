e5b762335d5d59be5ac7f1237edaf27496ebbb5e
Nimira43 <n.rabone72@aol.com>
2025-11-10 19:28:18 +0000

    camera

--- src/script.js
+++ src/script.js
@@ -28,3 +28,13 @@ const sizes = {
   height: window.innerHeight
 }
 
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 5
+scene.add(camera)
