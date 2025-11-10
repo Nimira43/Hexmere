c65250d0f797a8933773f6e4c8cefca9ce75eeb4
Nimira43 <n.rabone72@aol.com>
2025-11-10 19:39:28 +0000

    Also corrected typo on line 36

--- src/script.js
+++ src/script.js
@@ -33,7 +33,7 @@ window.addEventListener('resize', () => {
   sizes.height = window.innerHeight
   camera.aspect = sizes.width / sizes.height
   camera.updateProjectionMatrix()
-  renderer.setSize(sizes.width / sizes.height)
+  renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
