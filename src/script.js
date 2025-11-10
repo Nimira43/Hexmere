79880cb03be5c5052cb755bc6e873005f41b9002
Nimira43 <n.rabone72@aol.com>
2025-11-10 20:02:12 +0000

    Rotation of floor

--- src/script.js
+++ src/script.js
@@ -20,6 +20,8 @@ const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(20, 20),
   new THREE.MeshStandardMaterial()
 )
+
+floor.rotation.x = - Math.PI * 0.5
 scene.add(floor)
 
 const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
