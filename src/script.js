c7a7a4744d0342d348431529d6e8ad33fe9874de
Nimira43 <n.rabone72@aol.com>
2025-12-10 09:27:15 +0000

    Ghost 3 animation.

--- src/script.js
+++ src/script.js
@@ -278,7 +278,10 @@ const tick = () => {
   ghost2.position.x = Math.cos(ghost2Angle) * 5
   ghost2.position.z = Math.sin(ghost2Angle) * 5
   ghost2.position.y = Math.sin(ghost2Angle) * Math.sin(ghost2Angle * 2.34) *  Math.sin(ghost2Angle * 3.45)  
-    
+  const ghost3Angle = elapsedTime * 0.23
+  ghost3.position.x = Math.cos(ghost3Angle) * 6
+  ghost3.position.z = Math.sin(ghost3Angle) * 6
+  ghost3.position.y = Math.sin(ghost3Angle) * Math.sin(ghost3Angle * 2.34) *  Math.sin(ghost3Angle * 3.45)   
 
   controls.update()
   renderer.render(scene, camera)
