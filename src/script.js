ded1d91dbadc27ace174d2f1b859159bdda01204
Nimira43 <n.rabone72@aol.com>
2025-12-27 07:30:47 +0000

    material uniforms sunPosition

--- src/script.js
+++ src/script.js
@@ -308,6 +308,7 @@ sky.material.uniforms['turbidity'].value = 10
 sky.material.uniforms['rayleigh'].value = 3
 sky.material.uniforms['mieCoefficient'].value = 0.1
 sky.material.uniforms['mieDirectionalG'].value = 0.95
+sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95)
 
 const timer = new Timer()
 const tick = () => {
